09180c3b20a8b818a85c87b8835c80064e86f73b
UDOOboard <info@udoo.org>
2014-06-11 14:57:18 +0200

    Updated to AdkToolkit

--- UDOOAndroidADKDemoBidirectional (Android app project)/UDOOAndroidADKDemoBidirectional/src/org/udoo/androidadkdemobidirect/MainActivity.java
+++ UDOOAndroidADKDemoBidirectional (Android app project)/UDOOAndroidADKDemoBidirectional/src/org/udoo/androidadkdemobidirect/MainActivity.java
@@ -1,234 +1,99 @@
 package org.udoo.androidadkdemobidirect;
 
-import java.io.FileDescriptor;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import org.udoo.androidadkdemobidirect.R;
-
+import me.palazzetti.adktoolkit.AdkManager;
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.ParcelFileDescriptor;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
-public class MainActivity extends Activity implements Runnable{
+public class MainActivity extends Activity{
 
-	private static final String TAG = "UDOO_AndroidADKFULL";	 
-	private static final String ACTION_USB_PERMISSION = "org.udoo.androidadkdemobidirect.action.USB_PERMISSION";
- 
-	private UsbManager mUsbManager;
-	private PendingIntent mPermissionIntent;
-	private boolean mPermissionRequestPending;
+//	private static final String TAG = "UDOO_AndroidADKFULL";	 
 	
-	UsbAccessory mAccessory;
-	ParcelFileDescriptor mFileDescriptor;
-	FileInputStream mInputStream;
-	FileOutputStream mOutputStream;
+	private AdkManager mAdkManager;
 	
 	private ToggleButton buttonLED;
-	private static TextView distance;
-	private boolean running = false;
+	private TextView distance;
 	
-	// Receive the USB attached intent 
-	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (ACTION_USB_PERMISSION.equals(action)) {
-				synchronized (this) {
-					UsbAccessory accessory = (UsbAccessory)intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-					if (intent.getBooleanExtra(
-						UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-						openAccessory(accessory);
-					} else {
-						Log.d(TAG, "permission denied for accessory "+ accessory);
-					}
-					mPermissionRequestPending = false;
-				}
-			} else if (UsbManager.ACTION_USB_ACCESSORY_DETACHED.equals(action)) {
-				UsbAccessory accessory = (UsbAccessory)intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-				if (accessory != null && accessory.equals(mAccessory)) {
-					closeAccessory();
-				}
-			}
-		}
-	};
-	
-	@SuppressWarnings("deprecation")
+	private AdkReadTask mAdkReadTask;
+		
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-		filter.addAction(UsbManager.ACTION_USB_ACCESSORY_DETACHED);
-		registerReceiver(mUsbReceiver, filter);
- 
-		if (getLastNonConfigurationInstance() != null) {
-			mAccessory = (UsbAccessory) getLastNonConfigurationInstance();
-			openAccessory(mAccessory);
-		}
-		
 		setContentView(R.layout.activity_main);
+		
+		mAdkManager = new AdkManager((UsbManager) getSystemService(Context.USB_SERVICE));
+		
+//		register a BroadcastReceiver to catch UsbManager.ACTION_USB_ACCESSORY_DETACHED action
+		registerReceiver(mAdkManager.getUsbReceiver(), mAdkManager.getDetachedFilter());
+		
 		buttonLED = (ToggleButton) findViewById(R.id.toggleButtonLED);
-		distance = (TextView) findViewById(R.id.textView_distance);
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	public Object onRetainNonConfigurationInstance() {
-		if (mAccessory != null) {
-			return mAccessory;
-		} else {
-			return super.onRetainNonConfigurationInstance();
-		}
+		distance  = (TextView) findViewById(R.id.textView_distance);
+		
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
- 
-		if (mInputStream != null && mOutputStream != null) {
-			return;
-		}
-		//open the accessory from the accessory list
-		UsbAccessory[] accessories = mUsbManager.getAccessoryList();
-		UsbAccessory accessory = (accessories == null ? null : accessories[0]);
-		if (accessory != null) {
-			if (mUsbManager.hasPermission(accessory)) {
-				openAccessory(accessory);
-			} else {
-				synchronized (mUsbReceiver) {
-					if (!mPermissionRequestPending) {
-						mUsbManager.requestPermission(accessory,mPermissionIntent);
-						mPermissionRequestPending = true;
-					}
-				}
-			}
-		} else {
-			Log.d(TAG, "mAccessory is null");
-		}
+		mAdkManager.open();
+		
+		mAdkReadTask = new AdkReadTask();
+		mAdkReadTask.execute();
 	}
 	
 	@Override
 	public void onPause() {
 		super.onPause();	
+		mAdkManager.close();
+		
+		mAdkReadTask.pause();
+		mAdkReadTask = null;
 	}
  
 	@Override
 	public void onDestroy() {
-		closeAccessory();
-		unregisterReceiver(mUsbReceiver);
 		super.onDestroy();
-	}
-	
-	// open the accessory and open the input and output stream from the descriptor
-	// start also the thread that reads from Arduino
-	private void openAccessory(UsbAccessory accessory) {
-		mFileDescriptor = mUsbManager.openAccessory(accessory);
-		if (mFileDescriptor != null) {
-			mAccessory = accessory;
-			FileDescriptor fd = mFileDescriptor.getFileDescriptor();
-			mInputStream = new FileInputStream(fd);
-			mOutputStream = new FileOutputStream(fd);
-			
-			Thread thread = new Thread(null, this, "UDOO_ADK_readfrom");
-			thread.start();
-			
-			Toast.makeText(getApplicationContext(), "Accessory connected", Toast.LENGTH_SHORT).show();
-			Log.i(TAG, "openaccessory");
-		} 
-		else {
-			Toast.makeText(getApplicationContext(), "Accessory not connected", Toast.LENGTH_SHORT).show();
-		}
-	}
-	
-	// close the accessory
-	private void closeAccessory() {
-		Log.i(TAG, "closeaccessory");
-		try {
-			if (mFileDescriptor != null) {
-				mFileDescriptor.close();
-			}
-		} catch (IOException e) {
-		} finally {
-			mFileDescriptor = null;
-			mAccessory = null;
-			running = false;
-		}
+		unregisterReceiver(mAdkManager.getUsbReceiver());
 	}
 	
 	// ToggleButton method - send message to SAM3X
 	public void blinkLED(View v){
-		if (mAccessory != null) {
-			byte[] message = new byte[1];
-			if (buttonLED.isChecked()) { 
-				message[0] = (byte)1;
-			} else {
-				message[0] = (byte)0; 
-			}	
-			if (mOutputStream != null) {
-				try {
-					mOutputStream.write(message);
-				} catch (IOException e) {
-					Log.e(TAG, "write failed", e);
-				}
-			}
+		if (buttonLED.isChecked()) { 
+			// writeSerial() allows you to write a single char or a String object.
+			mAdkManager.writeSerial("1");
 		} else {
-			Toast.makeText(getApplicationContext(),
-		               "Accessory not connected", 
-		               Toast.LENGTH_SHORT).show();
+			mAdkManager.writeSerial("0"); 
 		}
 	}
-	 
 	
-	private Message m;
-	
-	// Thread for read data from SAM3X
-	public void run() {
-		int ret = 0;
-		byte[] buffer = new byte[4];
-		running = true;
-		
-		while (running) {
-			try {
-				ret = mInputStream.read(buffer);
-			} catch (IOException e) {
-				break;
-			}
-			m = Message.obtain(mHandler);
+	/* 
+	 * We put the readSerial() method in an AsyncTask to run the 
+	 * continuous read task out of the UI main thread
+	 */
+	private class AdkReadTask extends AsyncTask<Void, String, Void> {
+
+		private boolean running = true;
 			
-			if (ret != 0) {
-				m.arg1 = unsignedByteToInt(buffer[0]);
-				ret = 0;
-			}			
-			mHandler.sendMessage(m);		
+		public void pause(){
+			running = false;
 		}
+		 
+	    protected Void doInBackground(Void... params) {
+//	    	Log.i("ADK demo bi", "start adkreadtask");
+	    	while(running) {
+	    		publishProgress(mAdkManager.readSerial()) ;
+	     	}
+	    	return null;
+	    }
+
+	    protected void onProgressUpdate(String... progress) {
+	    	distance.setText((int)progress[0].charAt(0) + " cm");
+//	    	Log.i(TAG, "received: " + (int)progress[0].charAt(0));
+	    }  
 	}
-	
-	private int unsignedByteToInt(byte b){
-		return b & 0xFF;
-	}
-	
-	static Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			distance.setText(msg.arg1 + " cm ");			
-		}
-	};
 
 }
--- UDOOArduinoADKDemoBidirectional (Arduino sketch)/UDOOArduinoADKDemoBidirect/UDOOArduinoADKDemoBidirect.ino
+++ UDOOArduinoADKDemoBidirectional (Arduino sketch)/UDOOArduinoADKDemoBidirect/UDOOArduinoADKDemoBidirect.ino
@@ -2,65 +2,72 @@
 #include <stdio.h>
 #include <adk.h>
 
-int ledPin = 13;        
-int IRpin  = A0;            
+//definizione pin collegati al LED, sensori Ping e luce
+#define LED_PIN  13
+int IR_PIN  = A0;            
 
 // Accessory descriptor. It's how Arduino identifies itself to Android.
-char applicationName[] = "UDOOAndroidADKDemoBidirect"; // the app installed in Android
-char accessoryName[] = "UDOO_ADK_BIDIRECT"; // your Arduino Accessory name (Need to be the same defined in the Android App)
-char companyName[] = "Aidilab";  // manufacturer (Need to be the same defined in the Android App)
+char descriptionName[] = "UDOOAndroidADKDemoBidirect"; // the app installed in Android
+char modelName[] = "UDOO_ADK_BIDIRECT"; // your Arduino Accessory name (Need to be the same defined in the Android App)
+char manufacturerName[] = "Aidilab";  // manufacturer (Need to be the same defined in the Android App)
 
 // Make up anything you want for these
 char versionNumber[] = "1.0"; // version (Need to be the same defined in the Android App)
 char serialNumber[] = "1";
 char url[] = "http://www.aidilab.it"; // If there isn't any compatible app installed, Android suggest to visit this url
                                      
-
 USBHost Usb;
-ADK adk(&Usb, companyName, accessoryName, applicationName, versionNumber, url, serialNumber);
-                     
+ADK adk(&Usb, manufacturerName, modelName, descriptionName, versionNumber, url, serialNumber);
+
+#define RCVSIZE 1   
+uint8_t buf[RCVSIZE];
+uint32_t bytesRead = 0;
+
+uint8_t bufWrite[1];                     
 int distance = 0;
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(ledPin, OUTPUT);
-    cpu_irq_enable(); 
-    printf("\r\nADK demo start\r\n");
-    delay(200);
+    pinMode(LED_PIN, OUTPUT);
+    delay(500);
+    Serial.println("UDOO ADK BIDIRECT demo start...");
 }
 
-#define RCVSIZE 1
-
 void loop()
-{
-    uint8_t bufRead[RCVSIZE];
-    uint32_t nbread = 0;
-    uint8_t bufWrite[1];
-    
+{    
     Usb.Task();
     
     if (adk.isReady()) {      
-        adk.read(&nbread, RCVSIZE, bufRead); // read data into bufRead array
-        if (nbread > 0) {
-          if (bufRead[0] == 1) // compare received data
-            digitalWrite(ledPin, HIGH); // turn on light
-          else
-            digitalWrite(ledPin, LOW); // turn off light
+        adk.read(&bytesRead, RCVSIZE, buf); // read data into bufRead array
+        if (bytesRead > 0) {
+          if (parseCommand(buf[0]) == 1) {// compare received data
+            // Received "1" - turn on LED
+            digitalWrite(LED_PIN, HIGH);
+          } else if (parseCommand(buf[0]) == 0) {
+            // Received "0" - turn off LED
+            digitalWrite(LED_PIN, LOW); 
+          }
         }
-        distance = analogRead(IRpin);
-        if (distance < 100) { 
+        distance = analogRead(IR_PIN);
+        if (distance < 100) {  // 
           distance = 100;  
         } else if (distance > 900){
           distance = 900;
         }
         bufWrite[0] = (uint8_t)(2076/(distance - 11) + 4);  //  calculate the distance in centimeters
   
-        adk.write(sizeof(bufWrite), (uint8_t *)bufWrite); // write the distance to Android app
+        adk.write(sizeof(bufWrite), (uint8_t *)bufWrite); //write the distance to Android
+        Serial.println(bufWrite[0]);
     } else {
-      digitalWrite(ledPin, LOW);
+      digitalWrite(LED_PIN, LOW);
     }  
     
-    delay(50);
+    delay(10);
+}
+
+// the characters sent to Arduino are interpreted as ASCII, we decrease 48 to return to ASCII range.
+uint8_t parseCommand(uint8_t received) {
+  return received - 48;
 }
 
